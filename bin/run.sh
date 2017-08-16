01eaa0d7b00fe4987fcdb0014080d5ace3f17850
V-Kopio <ville.kopio@gmail.com>
2017-08-16 22:44:43 +0300

    Changed script again TM (c)

--- bin/run.sh
+++ bin/run.sh
@@ -6,6 +6,4 @@ python3 run.py
 
 cd /app/vagrant/ldap
 
-vagrant ssh -c 'sudo su && ldapdelete -r -h 127.0.0.1 -D "cn=admin,dc=satakuntatalo,dc=fi" -w $LDAPPW "ou=talolaiset,ou=saatio,ou=lists,dc=satakuntatalo,dc=fi"'
-vagrant ssh -c 'sudo su && ldapdelete -r -h 127.0.0.1 -D "cn=admin,dc=satakuntatalo,dc=fi" -w $LDAPPW "ou=osakehuoneistot,ou=saatio,ou=lists,dc=satakuntatalo,dc=fi"'
-vagrant ssh -c 'sudo su && ldapadd -h 127.0.0.1 -D "cn=admin,dc=satakuntatalo,dc=fi" -w $LDAPPW -f `ls -t /home/vagrant/ldif/*.ldif | head -1`'
+vagrant ssh -- -t './update-ldap.sh'
