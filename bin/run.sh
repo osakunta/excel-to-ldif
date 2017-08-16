14413eea15d1e4b8af6a64690e661f39962b2e5e
V-Kopio <ville.kopio@gmail.com>
2017-08-16 22:19:23 +0300

    Changed script again

--- bin/run.sh
+++ bin/run.sh
@@ -6,6 +6,6 @@ python3 run.py
 
 cd /app/vagrant/ldap
 
-vagrant ssh -t 'sudo su && ldapdelete -r -h 127.0.0.1 -D "cn=admin,dc=satakuntatalo,dc=fi" -w $LDAPPW "ou=talolaiset,ou=saatio,ou=lists,dc=satakuntatalo,dc=fi"'
-vagrant ssh -t 'sudo su && ldapdelete -r -h 127.0.0.1 -D "cn=admin,dc=satakuntatalo,dc=fi" -w $LDAPPW "ou=osakehuoneistot,ou=saatio,ou=lists,dc=satakuntatalo,dc=fi"'
-vagrant ssh -t 'sudo su && ldapadd -h 127.0.0.1 -D "cn=admin,dc=satakuntatalo,dc=fi" -w $LDAPPW -f `ls -t /home/vagrant/ldif/*.ldif | head -1`'
+vagrant ssh -- -t 'sudo su && ldapdelete -r -h 127.0.0.1 -D "cn=admin,dc=satakuntatalo,dc=fi" -w $LDAPPW "ou=talolaiset,ou=saatio,ou=lists,dc=satakuntatalo,dc=fi"'
+vagrant ssh -- -t 'sudo su && ldapdelete -r -h 127.0.0.1 -D "cn=admin,dc=satakuntatalo,dc=fi" -w $LDAPPW "ou=osakehuoneistot,ou=saatio,ou=lists,dc=satakuntatalo,dc=fi"'
+vagrant ssh -- -t 'sudo su && ldapadd -h 127.0.0.1 -D "cn=admin,dc=satakuntatalo,dc=fi" -w $LDAPPW -f `ls -t /home/vagrant/ldif/*.ldif | head -1`'
