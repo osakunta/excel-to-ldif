596e5be72fab13e05b00f2254ff1927a6e919744
V-Kopio <ville.kopio@gmail.com>
2017-08-17 21:17:17 +0300

    Changed mailing list structure to make sense

--- sample/ldif_parser.py
+++ sample/ldif_parser.py
@@ -1,30 +1,18 @@
 from data.data import ldif_file
 from sample.validator import *
 
-def print_ldif_header(f):
-    f.write('dn: ou=talolaiset,ou=saatio,ou=lists,dc=satakuntatalo,dc=fi\n')
-    f.write('objectClass: top\n')
-    f.write('objectClass: organizationalUnit\n')
-    f.write('ou: talolaiset\n')
-    f.write('\n')
-
-    f.write('dn: ou=osakehuoneistot,ou=saatio,ou=lists,dc=satakuntatalo,dc=fi\n')
-    f.write('objectClass: top\n')
-    f.write('objectClass: organizationalUnit\n')
-    f.write('ou: osakehuoneistot\n')
-    f.write('\n')
-
-def print_ldif_entry(f, cn, email, mail_list):
-    if not valid_email(email, False):
-        return
-
-    f.write('dn: cn=' + cn + ',ou=' + mail_list + ',ou=saatio,ou=lists,dc=satakuntatalo,dc=fi\n')
+def print_ldif_header(f, mail_list):
+    f.write('dn: cn=' + mail_list + ',ou=saatio,ou=lists,dc=satakuntatalo,dc=fi\n')
     f.write('objectClass: top\n')
     f.write('objectClass: extensibleObject\n')
     f.write('objectClass: organizationalRole\n')
-    f.write('cn: ' + cn + '\n')
+    f.write('cn: ' + mail_list + '\n')
+
+def print_ldif_entry(f, email):
+    if not valid_email(email):
+        return
+
     f.write('mail: ' + email + '\n')
-    f.write('\n')
 
 def get_emails(mail, apartment_entry):
     if mail in apartment_entry:
@@ -35,29 +23,25 @@ def get_emails(mail, apartment_entry):
 def apartment_emails(apartment_entry):
     return get_emails('email', apartment_entry) + get_emails('tenant', apartment_entry)
 
-def for_every_email_in(f, apartment, apartment_entry, mail_list):
+def for_every_email_in(f, apartment_entry):
     entries = apartment_emails(apartment_entry)
 
     for i, entry in enumerate(entries):
-        cn = apartment
+        print_ldif_entry(f, entry.strip())
 
-        # If there are multiple emails for one apartment, give unique cn
-        if i > 0:
-            cn += '_' + str(i + 1)
-
-        print_ldif_entry(f, cn, entry.strip(), mail_list)
-
-def create_entries_for_apartment(f, apartment, apartment_entry, mail_list):
-    for_every_email_in(f, apartment, apartment_entry, mail_list)
+def create_entries_for_apartment(f, apartment_entry):
+    for_every_email_in(f, apartment_entry)
 
 def write_ldif(residents):
     f = open(ldif_file, 'w')
 
-    print_ldif_header(f)
-
     for mail_list in residents:
+        print_ldif_header(f, mail_list)
+
         for apartment in residents[mail_list]:
             apartment_entry = residents[mail_list][apartment]
-            create_entries_for_apartment(f, apartment, apartment_entry, mail_list)
+            create_entries_for_apartment(f, apartment_entry)
+
+        f.write('\n')
 
     f.close()
